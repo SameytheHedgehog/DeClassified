a0d9b4fce6bdcad30c4aad67008e33f7fd5941fc
SameytheHedgehog <superzander369@gmail.com>
2022-05-17 04:07:49 -0500

    sick ass about page by Eris
    It's real awesome. :3

---
+++ about/WACUPDetect_about.m
@@ -0,0 +1,21 @@
+#include "..\..\..\lib/std.mi"
+#include "..\scripts\IsWACUP.m"
+
+Global group frameGroup;
+Global Text WACUPtxt;
+
+Function DetectWACUP();
+
+System.onScriptLoaded(){
+
+    initDetector();
+    frameGroup = getScriptGroup();
+
+    WACUPtxt = frameGroup.findObject("isthiswacup");
+
+    if(IsWACUP == 1){
+        WACUPtxt.setText("Is this WACUP?: Yes");
+    }else if(IsWACUP == 0){
+        WACUPtxt.setText("Is this WACUP?: No");
+    }
+}
