dee4920347c373ac28df5812c67163869a0f658e
SameytheHedgehog <superzander369@gmail.com>
2022-05-24 16:47:07 -0500

    removed the previous scrollbar changes and instead editted the playlists appearance.

--- scripts/WACUPDetect.m
+++ scripts/WACUPDetect.m
@@ -5,8 +5,7 @@
 #include "compiler/lib/std.mi"
 #include "IsWACUP.m"
 
-Global GuiObject WAPLC;
-Global Group frameGroup, frameGroupShade, frameGroupPL;
+Global Group frameGroup, frameGroupShade;
 
 Global Layer wacupmain, wacuptitlebar, wacupshade;
 
@@ -16,14 +15,11 @@ System.onScriptLoaded(){
 
     frameGroup = getContainer("Main").getLayout("Normal");
 	frameGroupShade = getContainer("Main").getLayout("shade");
-    frameGroupPL = getContainer("PL").getLayout("normal");
 
     wacupshade = frameGroupShade.findObject("washade");
 
     wacupmain = frameGroup.findObject("mainwnd");
 	wacuptitlebar = frameGroup.findObject("wa.titlebar");
-
-	WAPLC = frameGroupPL.findObject("wasabi.list");
 
 	if(IsWACUP != 0){
 		wacupmain.setXmlParam("image", "wacup.main");
@@ -31,15 +27,11 @@ System.onScriptLoaded(){
 		wacuptitlebar.setXmlParam("inactiveimage", "wacup.titlebar.off");
 		wacupshade.setXmlParam("image", "wacup.player.shade.enabled");
 		wacupshade.setXmlParam("inactiveimage", "wacup.player.shade.disabled");
-		WAPLC.setXmlParam("x", "12");
-		WAPLC.setXmlParam("w", "-31");
 	}else{
 		wacupmain.setXmlParam("image", "wa.main");
 		wacuptitlebar.setXmlParam("image", "wa.titlebar.on");
 		wacuptitlebar.setXmlParam("inactiveimage", "wa.titlebar.off");
 		wacupshade.setXmlParam("image", "wa2.player.shade.enabled");
 		wacupshade.setXmlParam("inactiveimage", "wa2.player.shade.disabled");
-		WAPLC.setXmlParam("x", "10");
-		WAPLC.setXmlParam("w", "-15");
 	}
 }
